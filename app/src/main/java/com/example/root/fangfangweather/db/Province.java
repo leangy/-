02d20cb9faa71e7a718e1042c65e034252bd0cc2
leangy <2861000621@qq.com>
2019-04-02 19:57:52 +0800

    First commit

---
+++ app/src/main/java/com/example/root/fangfangweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.root.fangfangweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by root on 2019/3/29.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+
+}
